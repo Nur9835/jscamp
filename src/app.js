3a500239d6d547a533fac1f3d3d8da3d3c1e7dc5
Nur <90011276+Nur9835@users.noreply.github.com>
2021-11-17 22:34:08 +0300

    17.11.21

---
+++ src/app.js
@@ -0,0 +1,23 @@
+console.log("merhaba kodlama")
+
+let dolarDun=9.20
+const euroDun=11
+
+{ let dolarDun=0}
+let dolarBugun= 9.30
+console.log(dolarDun)
+
+
+
+let konutKredileri=["Konut kredisi","Kamu Konut Kredisi","Emlak Konut Kredisi"]
+console.log("<ul>")
+for (let index = 0; index < konutKredileri.length; index++) {
+   console.log("<li>"+konutKredileri[index]+ "/li>")
+    
+} 
+
+console.log("</ul>")
+
+
+
+console.log(konutKredileri)
